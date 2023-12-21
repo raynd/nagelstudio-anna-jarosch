ee01e48138ebf61368443d9c4f452fbd9c934793
Andreas Kuthe <andreas.kuthe@googlemail.com>
2023-12-21 11:08:34 +0100

    update styleguide and typography

--- src/app/styleguide/typography/page.tsx
+++ src/app/styleguide/typography/page.tsx
@@ -2,8 +2,10 @@ import { getStyleguideDataTypography } from '@/app/utils'
 import {
   table,
   typefaceName,
+  typefaceSize,
   typefaceSample,
   typefaceLegend,
+  grid,
 } from '../styleguide.module.scss'
 
 type TObject = {
@@ -26,42 +28,55 @@ export default async function Styleguide() {
     <main>
       {data.map((record, index) => {
         const key = Object.keys(record)[0]
-        const { cssClass, size } = record[key]
+        const { cssClass, size, name, description } = record[key]
 
         return (
-          <div key={index}>
-            <p className={`pica-strong ${typefaceName}`}>{key}</p>
-            <p className={`${typefaceSample} ${cssClass}`}>
-              ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz 0123456789
-            </p>
-            <table className={`${table} ${typefaceLegend}`}>
-              <thead>
-                <tr>
-                  <td>
-                    <span className="minion">name</span>
-                  </td>
-                  <td>
-                    <span className="minion">className</span>
-                  </td>
-                  <td>
-                    <span className="minion">size</span>
-                  </td>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>
-                    <span className="minion-strong">{key}</span>
-                  </td>
-                  <td>
-                    <span className="minion-strong">{cssClass}</span>
-                  </td>
-                  <td>
-                    <span className="minion-strong">{size}</span>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+          <div key={index} className={grid}>
+            <div>
+              <p className={`brevier-strong ${typefaceName}`}>{name}</p>
+              <p className={`${cssClass} ${typefaceSize}`}>45/23</p>
+
+              <table className={`${table} ${typefaceLegend}`}>
+                <thead>
+                  <tr>
+                    <td>
+                      <span className="minion">name</span>
+                    </td>
+                    <td>
+                      <span className="minion">className</span>
+                    </td>
+                    <td>
+                      <span className="minion">size</span>
+                    </td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      <span className="minion-strong">{name}</span>
+                    </td>
+                    <td>
+                      <span className="minion-strong">{cssClass}</span>
+                    </td>
+                    <td>
+                      <span className="minion-strong">{size}</span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div>
+              <p className={`brevier-strong ${typefaceName}`}>&nbsp;</p>
+              <p className={`${typefaceSample} ${cssClass}`}>
+                Everyone has the right to freedom of thought, conscience and
+                religion; this right includes freedom to change his religion or
+                belief...
+                <br />
+                <br />
+                ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz 0123456789
+                #§&?!/*#-
+              </p>
+            </div>
           </div>
         )
       })}
